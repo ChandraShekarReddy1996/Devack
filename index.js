3d38e19588d30aa08655000fa120c764253a3115
socomo <socomo@soc-macmini-17.local>
2018-08-03 16:16:52 +0530

    Testing the Http module

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ var express = require('express');
 var mongoose = require('mongoose');
 
 //Mongoose connection
-mongoose.Promise = global.Promise;
+//mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/Devack');
-console.log('Mongoose is Sucessfully connected');
+
 
 var adminkey =  'jshfiaohyfsoinqwlk23lj4g3u224d'
 //Encoder Signature for JSON Web Token
@@ -19,3 +19,9 @@ module.exports.keys = {
   Encoder : 'RESTFULAPI',
   adminkey : adminkey
 }
+
+var http = require('http')
+
+http.createServer((req,res) => {
+res.end('Hello World')
+}).listen(3002)
