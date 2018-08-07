6d8864007858c1fa3cb3eb9d4644f354a517c43d
socomo <socomo@soc-macmini-17.local>
2018-08-07 14:24:35 +0530

    Testing routes

--- index.js
+++ index.js
@@ -19,9 +19,16 @@ module.exports.keys = {
   Encoder : 'RESTFULAPI',
   adminkey : adminkey
 }
+var express = require('express')
+var bodyParser = require('body-parser');
+var ctr = require('../ctrls/adminCtrls').admin;
+var fileUpload = require('express-fileupload');
 
-var http = require('http')
+var app = express();
 
-http.createServer((req,res) => {
-res.end('Hello World')
-}).listen(3002)
+app.post('/registerMember',jsonParser,(req,res) => {
+  console.log('hello World')
+  ctr.registerMember(req,res);
+})
+
+app.listen(3000)
